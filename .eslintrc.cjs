d103cf130a50050a90655f6e381bffa324864968
David Lu <davidlu1111@gmail.com>
2023-08-14 20:10:12 -0700

    starter stuff

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,45 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2021: true },
+  settings: {
+    react: {
+      version: "detect",
+    },
+    "import/resolver": {
+      typescript: {}, // this loads <rootdir>/tsconfig.json to eslint
+    },
+  },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:prettier/recommended",
+    "plugin:import/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:import/typescript",
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: [
+    "react",
+    "@typescript-eslint",
+    "eslint-plugin-canonical",
+    "import",
+    "simple-import-sort",
+    "unused-imports",
+  ],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "tsconfig.json",
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 13,
+    sourceType: "module",
+  },
+  plugins: ["react-refresh"],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
+    "react-refresh/only-export-components": [
+      "warn",
       { allowConstantExport: true },
     ],
   },
-}
+};
---
+++ postcss.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  plugins: {
+    autoprefixer: {},
+    cssnano: {},
+    "postcss-nesting": { noIsPseudoSelector: false },
+    "postcss-custom-media": { preserve: false },
+  },
+};
